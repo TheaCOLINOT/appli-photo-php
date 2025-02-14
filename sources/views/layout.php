ea3ca0516ed811c314a8b069418ff2d0bb2444f6
Mariam Bouhassoune <151145091+MaraimBouhassoune@users.noreply.github.com>
2025-02-14 19:57:04 +0100

    Correction de la page groupe manage

--- sources/views/layout.php
+++ sources/views/layout.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Site</title>
-    <link rel="stylesheet" href="../public/assets/css/main.css">
+    <link rel="stylesheet" href="/public/assets/css/main.css">
+    
 </head>
 <body>
     <header class="header">
