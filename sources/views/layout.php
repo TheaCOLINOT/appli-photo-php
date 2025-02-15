1ba44cb231f39cb5ef4adb676685c07d80d1ee8b
Mariam Bouhassoune <151145091+MaraimBouhassoune@users.noreply.github.com>
2025-02-16 00:18:51 +0100

    Correction problem page

--- sources/views/layout.php
+++ sources/views/layout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Site</title>
-    <link rel="stylesheet" href="dist/framework-esgi.css">
+    <link rel="stylesheet" href="/dist/framework-esgi.css">
 
 </head>
 
