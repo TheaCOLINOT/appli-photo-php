6b4cad1b9b0ed1a587e17873e8466127066e8185
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2025-02-16 17:32:55 +0100

    add style for login page

--- sources/views/login/index.php
+++ sources/views/login/index.php
@@ -7,41 +7,46 @@
       <?php echo Session::getError('auth'); ?>
     </div>
   <?php endif; ?>
-  <figure>
-    <img src="assets\scss/images/img1.jpg" alt="image présentation" class="img1">
-  </figure>
-  <form method="POST" action="/login">
-    <div class="input">
-      <label class="block text-gray-700">Email</label>
-      <input type="email" name="email" class="input"
-        value="<?php echo htmlspecialchars(Session::getOldInput('email')); ?>">
-      <?php if (Session::hasError('email')): ?>
-        <p class="text-red-500 text-sm"><?php echo Session::getError('email'); ?></p>
-      <?php endif; ?>
-    </div>
+  <div class="form form--div">
+    <!-- <figure> -->
+      <img src="/public/img1.jpg" alt="image présentation" class="image image--md image--square">
+    <!-- </figure> -->
+    <form class="form--md" method="POST" action="/login">
 
-    <div class="input">
-      <label class="block text-gray-700">Mot de passe</label>
-      <input type="password" name="password" class="input">
-      <?php if (Session::hasError('password')): ?>
-        <p class="text-red-500 text-sm"><?php echo Session::getError('password'); ?></p>
-      <?php endif; ?>
-    </div>
+      <div class="input--div">
+        <label class="input--label">Email</label>
+        <input type="email" name="email" class="input input--lg"
+          value="<?php echo htmlspecialchars(Session::getOldInput('email')); ?>">
+        <?php if (Session::hasError('email')): ?>
+          <p class="text-red-500 text-sm"><?php echo Session::getError('email'); ?></p>
+        <?php endif; ?>
+      </div>
 
-    <div class="flex items-center justify-between mb-6">
-      <button type="submit" class="button button--primary">
-        Se connecter
-      </button>
-      <a href="/password-reset" class="button button--secondary">
-        Mot de passe oublié ?
-      </a>
-    </div>
-  </form>
+      <div class="input--div">
+        <label class="input--label">Mot de passe</label>
+        <input type="password" name="password" class="input input--lg">
+        <?php if (Session::hasError('password')): ?>
+          <p class="text-red-500 text-sm"><?php echo Session::getError('password'); ?></p>
+        <?php endif; ?>
+      </div>
+
+      <div class="flex items-center justify-between mb-6">
+        <button type="submit" class="button button--primary">
+          Se connecter
+        </button>
+        <a href="/password-reset" class="button button--secondary">
+          Mot de passe oublié ?
+        </a>
+      </div>
+      <p class="text-center text-gray-600 text-sm">
+        Pas encore de compte ? <a href="/register" class="text-blue-500 hover:text-blue-700">S'inscrire</a>
+      </p>
+    </form>
+    
+  </div>
 
 
-  <p class="text-center text-gray-600 text-sm">
-    Pas encore de compte ? <a href="/register" class="text-blue-500 hover:text-blue-700">S'inscrire</a>
-  </p>
+  
 </div>
 <?php
 $content = ob_get_clean();
