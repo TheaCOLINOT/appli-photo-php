ca4ed6a71842187711e3f2c3af055a496d26d65d
Mariam Bouhassoune <151145091+MaraimBouhassoune@users.noreply.github.com>
2025-02-16 20:35:48 +0100

    Update manage.php

--- sources/views/upload/manage.php
+++ sources/views/upload/manage.php
@@ -5,8 +5,6 @@
     <!-- Lien ou bouton pour ajouter une nouvelle photo -->
 
     <div class="container mx-auto px-4 py-6 mb-8">
-            <h1 class="text-3xl font-bold mb-6">Uploader une photo</h1>
-
             <form action="/upload" method="post" enctype="multipart/form-data" class="form">
                 <label class="block text-gray-700 mb-2">Choisissez une image :</label>
                 <input type="file" name="photo" accept="image/*" class="input">
