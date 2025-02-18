0dc56afc54e5996f8e6ab2956885617f6a3768a9
Mariam Bouhassoune <151145091+MaraimBouhassoune@users.noreply.github.com>
2025-02-19 00:54:13 +0100

    Update HomeController.php

--- sources/controllers/HomeController.php
+++ sources/controllers/HomeController.php
@@ -6,12 +6,4 @@ class HomeController
     {
         require_once __DIR__ . '/../views/frontend/accueil.php';
     }
-    public static function galerie(): void
-    {
-        require_once __DIR__ . '/../views/frontend/galerie.php';
-    }
-    public static function mesphotos(): void
-    {
-        require_once __DIR__ . '/../views/frontend/mesphotos.php';
-    }
 }
