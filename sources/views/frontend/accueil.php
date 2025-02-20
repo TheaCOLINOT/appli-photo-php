e5ed48dad26cbda88ee59b6d99e594cbf783f3d3
Aka <94462079+JoelAka01@users.noreply.github.com>
2025-02-20 23:12:08 +0100

    groupe && tableau

--- sources/views/frontend/accueil.php
+++ sources/views/frontend/accueil.php
@@ -56,9 +56,53 @@
                     <img src="/public/image3.png" class="image image--md " alt="image présentation">
                 </div>  
         </div>
-
-
- 
+    </div>
+</section>
+<section >
+    <h2 class="title">Cart</h2>
+    <div class="apropos  container">
+        <div class="apropos--item grid">
+                <div class="apropos--content--text col-lg-6 col-md-6 col-12 col-xxl-6">
+                    <p>Votre premier Application de photo Grouper partager 
+                        entre amis Votre premier Application de photo Grouper partager entre amis
+                        Votre premier Application de photo Grouper partager entre amis Votre premier A
+                        pplication de photo Grouper partager entre amis Votre premier Application de photo Grouper partager entre amis Votre premier Application de photo Grouper partager
+                        entre amis Votre premier Application de photo Grouper partager entre amis 
+                    </p>
+                    <a href="/login">En savoir plus</a>
+                </div>
+                <div class="col-lg-6 col-md-6 apropos--content--image col-xl-6 col-xxl-6" >
+                    <img src="/public/image1.png" class="image image--md " alt="image présentation">
+                </div>  
+        </div>
+        <div class="apropos--item--revers  grid">
+                <div class="apropos--content--text col-lg-6 col-md-6 col-12 col-xxl-6">
+                    <p>Votre premier Application de photo Grouper partager 
+                        entre amis Votre premier Application de photo Grouper partager entre amis
+                        Votre premier Application de photo Grouper partager entre amis Votre premier A
+                        pplication de photo Grouper partager entre amis Votre premier Application de photo Grouper partager entre amis Votre premier Application de photo Grouper partager
+                        entre amis Votre premier Application de photo Grouper partager entre amis 
+                    </p>
+                    <a href="/login">En savoir plus</a>
+                </div>
+                <div class="col-lg-6 col-md-6 apropos--content--image col-xl-6 col-xxl-6" >
+                    <img src="/public/image2.png" class="image image--md " alt="image présentation">
+                </div>  
+        </div>        
+        <div class="apropos--item grid">
+                <div class="apropos--content--text  col-lg-6 col-md-6 col-12 col-xxl-6">
+                    <p>Votre premier Application de photo Grouper partager 
+                        entre amis Votre premier Application de photo Grouper partager entre amis
+                        Votre premier Application de photo Grouper partager entre amis Votre premier A
+                        pplication de photo Grouper partager entre amis Votre premier Application de photo Grouper partager entre amis Votre premier Application de photo Grouper partager
+                        entre amis Votre premier Application de photo Grouper partager entre amis 
+                    </p>
+                    <a href="/login">En savoir plus</a>
+                </div>
+                <div class="col-lg-6 col-md-6 apropos--content--image col-xl-6 col-xxl-6" >
+                    <img src="/public/image3.png" class="image image--md " alt="image présentation">
+                </div>  
+        </div>
     </div>
 </section>
 <?php
