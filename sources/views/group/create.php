d064982e08d3627b6c75355095da03b1edeb7013
Mariam Bouhassoune <151145091+MaraimBouhassoune@users.noreply.github.com>
2025-02-21 02:07:04 +0100

    Update create.php

--- sources/views/group/create.php
+++ sources/views/group/create.php
@@ -2,9 +2,9 @@
 <div class="container">
     <h1 class="text-3xl font-bold mb-6">Créer un groupe</h1>
     <form action="/group/create" method="post" class="form">
-        <div class="mb-4">
+        <div class="input--div">
             <label for="name" class="block text-gray-700 font-medium">Nom du groupe :</label>
-            <input type="text" name="name" id="name" class="w-full border rounded px-3 py-2" placeholder="Nom du groupe" required>
+            <input type="text" name="name" id="name" class="input  input--lg" placeholder="Nom du groupe" required>
         </div>
         <button type="submit" class="button button--sm button--primary">Créer le groupe</button>
     </form>
